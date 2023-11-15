2d926e5cca5c4128702d3149aeb2bee96a720417
dusnazarov.ed@gmail.com <dusnazarov.ed@gmail.com>
2023-11-15 09:25:16 -0600

    second commit

---
+++ 5_2_nodeJS_build_web_server/server.js
@@ -0,0 +1,39 @@
+
+const http = require('http');
+const path = require('path')
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {};
+
+// initialize object
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+
+    console.log(req.url, req.method);
+    let path;
+
+    if (req.url === '/' || req.url === 'index.html') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        path = path.join(__dirname, 'views', 'index.html');
+        fs.readFile(path, 'utf8',(err, data) => {
+            res.end(data);
+        });
+    }
+} )
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// myEmitter.on('log', (msg) => logEvents(msg));   
+//     myEmitter.emit('log', 'Log event emitted!');
+
+
+
